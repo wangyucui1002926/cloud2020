011c40997ed102dfa1a1344a34e879caab72711c
wangyucui <972013699@qq.com>
2020-08-25 10:34:16 +0800

    初始化

---
+++ cloud-provider-payment8001/src/main/java/com/example/springcloud/PaymentMain8001.java
@@ -0,0 +1,17 @@
+package com.example.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Classname PaymentMain8001
+ * @Description TODO
+ * @Date 8/21/2020 5:35 PM
+ * @Created by yucui.wang
+ */
+@SpringBootApplication
+public class PaymentMain8001 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain8001.class,args);
+    }
+}
---
+++ cloud-provider-payment8001/src/main/java/com/example/springcloud/entities/Pament.java
@@ -0,0 +1,10 @@
+package com.example.springcloud.entities;
+
+/**
+ * @Classname Pament
+ * @Description TODO
+ * @Date 8/21/2020 5:53 PM
+ * @Created by yucui.wang
+ */
+public class Pament {
+}
