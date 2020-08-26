70825a6b4fa5d7e4642e9d3a26bdde2beb9f0a69
wangyucui <972013699@qq.com>
2020-08-26 23:06:20 +0800

    工程重构

--- cloud-provider-payment8001/src/main/java/com/example/springcloud/dao/PaymentDao.java
+++ cloud-provider-payment8001/src/main/java/com/example/springcloud/dao/PaymentDao.java
@@ -1,7 +1,6 @@
 package com.example.springcloud.dao;
 
 import com.example.springcloud.entities.Payment;
-import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
 
