421360146555c27a062c4cd0f58a6cd246e35674
wangyucui <972013699@qq.com>
2020-08-27 15:29:06 +0800

    Eureka Server 集群构建

---
+++ cloud-eureka-server7002/src/main/java/springcloud/EurekaMain7002.java
@@ -0,0 +1,18 @@
+package springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @Description TODO
+ * @Author Wang Yucui
+ * @Date 8/27/2020 9:18 AM
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaMain7002 {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaMain7002.class, args);
+    }
+}
